6bf4caa7dbf841dc4cef5a3b9de6fabae600919b
Nathanael Blocher <blocher.nathanael@gmail.com>
2023-05-11 07:26:04 -0700

    regular update

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+
+// declared an empty object
+let obj = {Something: 15}
+let key = "Something"
+
+if (!obj[key]) {
+    // this code executes if obj[key] is undefined
+    console.log("obj[key] is undefined")
+} else {
+    // this code executes if obj[key] exists
+    console.log(obj[key])
+}
+
+
